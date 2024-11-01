4c2a0c81698b66b1342a855386c1f87834cc1887
joey <jmackle@ufl.edu>
2024-11-01 14:18:27 -0400

    profile page 1

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,28 @@
-export default function Profile() {
+//import { getProfile } from '@/components/get-profile'
+import { getServerUser } from "@/lib/getServerUser";
+
+/*export async function generateMetadata({ params }: { params: { username: string } }) {
+  const profile = await getProfile(params.username);
+  return {
+    title: profile?.name || 'NourishNetwork',
+  };
+}*/
+
+export default async function Page() {
+  const [user] = await getServerUser();
+  //const profile = await getProfile(params.username);
+  //const isLoggedIn = user?.id === profile?.id;
+
   return (
-    <>
-      <h1> Profile </h1>
-    </>
+    <div>
+      {user && (
+        <div className="mt-4">
+          {user.username}
+          {user.email}
+          {user.id}
+          {user.group}
+        </div>
+      )}
+    </div>
   );
 }
