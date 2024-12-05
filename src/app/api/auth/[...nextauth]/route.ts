d9d133083fe04beb9f4b23776b77909b696e8669
joey <jmackle@ufl.edu>
2024-12-05 12:19:58 -0500

    nextauth_url (fix)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,3 @@
-/*import NextAuth from "next-auth";
-import { authOptions } from "@/auth";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };*/
 import NextAuth, { SessionStrategy } from "next-auth";
 import { Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
--- src/components/UpcomingEvents.tsx
+++ src/components/UpcomingEvents.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default async function UpcomingEvents({ zipCode }: { zipCode: string }) {
-  const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000"; // localhost fallback
+  const env = process.env.NODE_ENV;
+  const baseUrl =
+    env === "production"
+      ? process.env.NEXTAUTH_URL_PRODUCTION
+      : process.env.NEXTAUTH_URL; // localhost fallback
   const apiUrl = `${baseUrl}/api/events?zipCode=${zipCode}`;
 
   // fetch events based on zip code
